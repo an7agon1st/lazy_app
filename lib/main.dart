17cfa66e819dffc2960a6403f737df500b13993f
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2021-06-03 19:05:21 +0530

    Added firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,51 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lazy_app/widgets/lazyAppBar.dart';
 import 'package:lazy_app/widgets/lazyExpansionTile.dart';
 import 'package:lazy_app/widgets/lazySwitchTile.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
+}
+
+class FirebaseInitialize extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Scaffold(
+            appBar: LazyAppBar(),
+            body: Center(
+              child: Text('Please chec your internet and restart app'),
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Scaffold(
+          appBar: LazyAppBar(),
+          body: Center(
+            child: Text('Make this better'),
+          ),
+        );
+        ;
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -26,37 +66,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  "LazyApp",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              ),
-              DefaultTextStyle(
-                style: const TextStyle(
-                    fontSize: 10.0, fontFamily: 'Agne', color: Colors.black),
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 5.0),
-                  child: AnimatedTextKit(
-                    repeatForever: true,
-                    stopPauseOnTap: true,
-                    animatedTexts: [
-                      TypewriterAnimatedText(
-                          'A reason to not get off your ass'),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      appBar: LazyAppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
